280ce874e1f54915bdf4906c172a1748313c3817
Atlas Research <aguennoune@outlook.com>
2025-06-05 20:42:02 +0100

    Updated khc_cli/version to v1.17.1

--- khc_cli/version.py
+++ khc_cli/version.py
@@ -1,5 +1,5 @@
 """Version information for the khc_cli package."""
 
-__version__ = "1.17.0"
+__version__ = "1.17.1"
 __author__ = "Abderrahim Guennoune"
 __author_email__ = "aguennoune@outlook.com"
